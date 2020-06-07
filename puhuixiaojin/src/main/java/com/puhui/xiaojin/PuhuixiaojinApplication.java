6cb986acbaa8c35853af295ed0fd4960818f2c78
wenbo <meitao123@dingtalk.com>
2020-06-07 18:03:07 +0800

    first commit

---
+++ puhuixiaojin/src/main/java/com/puhui/xiaojin/PuhuixiaojinApplication.java
@@ -0,0 +1,13 @@
+package com.puhui.xiaojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuhuixiaojinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuhuixiaojinApplication.class, args);
+	}
+
+}
---
+++ puhuixiaojin/src/test/java/com/puhui/xiaojin/PuhuixiaojinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puhui.xiaojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuhuixiaojinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
