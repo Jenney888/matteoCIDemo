8079f9cdaf7917bcca6e3261618e59df3a910bdb
wenbo <meitao123@dingtalk.com>
2020-06-09 22:08:44 +0800

    ++ commit002

--- puhuixiaojin/src/main/java/com/puhui/xiaojin/PuhuixiaojinApplication.java
+++ puhuixiaojin/src/main/java/com/puhui/xiaojin/PuhuixiaojinApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PuhuixiaojinApplication {
 
 	public static void main(String[] args) {
+		System.out.println("args +\"\" = " + args +"项目初始化...");
 		SpringApplication.run(PuhuixiaojinApplication.class, args);
 	}
 
